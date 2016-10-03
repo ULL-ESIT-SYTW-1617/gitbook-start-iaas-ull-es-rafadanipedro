b6b61c9d410720d7436114e502973e212bd5a7cc
Daniel Ramos <danielramosacosta@hotmail.com>
2016-10-03 17:41:59 +0100

    Creada la clase instanciadora

---
+++ src/gitbookStart.js
@@ -0,0 +1,10 @@
+export default class Gitbook {
+  constructor (options) {
+    console.log('Las opciones que me has pasado son:')
+    console.log(options)
+  }
+
+  write () {
+    console.log('Debería escribir los ficheros')
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import minimist from 'minimist'
+import Gitbook from './gitbookStart'
 
 const argv = minimist(process.argv.slice(2))
 
 console.dir(argv);
+
+let options = {
+  autor: 'Pepe Ramirez'
+}
+
+let gitbook = new Gitbook(options)
+gitbook.write()
