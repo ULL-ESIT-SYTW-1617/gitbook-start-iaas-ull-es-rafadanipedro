c49a39726eecb5f0d11e2765d06f17a86e41ee11
Daniel Ramos <danielramosacosta@hotmail.com>
2016-10-03 19:22:57 +0100

    Arreglando el código y quitando las pruebas

--- src/gitbookStart.js
+++ src/gitbookStart.js
@@ -4,33 +4,16 @@ import path from 'path'
 
 export default class Gitbook {
   constructor (options) {
-    console.log('Las opciones que me has pasado son:')
-    console.log(options)
-    // this.options = options
-    // Para probar:
-    this.options = {
-      author: 'Pepe',
-      email: 'pepe@pepe.com',
-      license: 'MIT',
-      repo: 'https://github.com/ULL-ESIT-SYTW-1617/tareas-iniciales-rafadanipedro',
-      ghPages: 'http://ULL-ESIT-SYTW-1617.github.io/tareas-iniciales-rafadanipedro',
-      name: 'mi-libro-fantastico',
-      title: 'Título del Gitbook',
-      description: 'Descripción breve del Gitbook',
-      outputDirName: 'output'
-    }
+    this.options = options
   }
 
   async write () {
-    console.log('Debería escribir los ficheros')
-    let file = await fs.readFile('template/package.json.ejs', {encoding: 'utf8'})
-    let output = ejs.render(file, this.options)
-    await this.renderTempalte('template', this.options)
+    await this.renderTempalte('template', this.options.outputDirName, this.options)
   }
 
-  async renderTempalte (directory, options) {
+  async renderTempalte (origin, dest, options) {
     try {
-      await fs.mkdir(directory.replace(/template/, options.outputDirName))
+      await fs.mkdir(dest)
     } catch (err) {
       if (err.message.match(/file already exists/)) {
         throw new Error(`El directorio ${err.message.match(/'(.+)'/)[1]} ya existe`)
@@ -38,21 +21,21 @@ export default class Gitbook {
         throw err
       }
     }
-    let files = await fs.readdir(directory)
+    let files = await fs.readdir(origin)
     let promises = []
     for(let file of files) {
-      let filePath = path.resolve(directory, file)
-      let outputFile = filePath.replace(/template/, options.outputDirName)
-      let stat = await fs.stat(filePath)
+      let originPath = path.resolve(origin, file)
+      let outputPath = path.resolve(dest, file)
+      let stat = await fs.stat(originPath)
       if (stat.isDirectory()) {
-        promises.push(this.renderTempalte(filePath, options))
+        promises.push(this.renderTempalte(originPath, outputPath, options))
       } else {
-        let output = await fs.readFile(filePath, {encoding: 'utf8'})
-        if (filePath.match(/\.ejs$/)) {
-          outputFile = outputFile.slice(0, -4)
+        let output = await fs.readFile(originPath, {encoding: 'utf8'})
+        if (originPath.match(/\.ejs$/)) {
+          outputPath = outputPath.slice(0, -4)
           output = ejs.render(output, this.options)
         }
-        promises.push(fs.writeFile(outputFile, output))
+        promises.push(fs.writeFile(outputPath, output))
       }
     }
     return Promise.all(promises)
